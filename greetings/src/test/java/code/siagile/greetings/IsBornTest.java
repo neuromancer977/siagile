19ae0d1830d675907e69bb977c7d15db45d6123e
luca.marrocco <luca.marrocco@27192eda-d5f7-11dd-8ca8-e3ff79f713b6>
2009-01-15 22:24:23 +0000

    update compare date without year

--- greetings/src/test/java/code/siagile/greetings/IsBornTest.java
+++ greetings/src/test/java/code/siagile/greetings/IsBornTest.java
@@ -10,7 +10,7 @@ import org.junit.*;
 public class IsBornTest {
 	@Test
 	public void testEmployeeIsBornToday() {
-		assertThat(employee("Doe, John, 1982/10/08, jonh.doe@foobar.com"), is(born("1982/10/08")));
-		assertThat(employee("Doe, John, 0000/00/00, jonh.doe@foobar.com"), is(not(born("1982/10/08"))));
+		assertThat(employee("Doe, John, 1982/10/08, jonh.doe@foobar.com"), is(born("2008/10/08")));
+		assertThat(employee("Doe, John, 0000/00/00, jonh.doe@foobar.com"), is(not(born("2008/10/08"))));
 	}
 }

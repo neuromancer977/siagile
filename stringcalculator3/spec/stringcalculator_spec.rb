87461b6358c6a57b11894c0c9080ae4c18a4f231
luca.marrocco <luca.marrocco@27192eda-d5f7-11dd-8ca8-e3ff79f713b6>
2009-12-09 18:14:05 +0000

    spec housekeeping

--- stringcalculator3/spec/stringcalculator_spec.rb
+++ stringcalculator3/spec/stringcalculator_spec.rb
@@ -1,87 +1,38 @@
 require 'stringcalculator'
 
-Spec::Matchers.define :add_to do |expected|
-  match do |string|
-    string.extend(StringCalculator).add.should == expected
-  end
+def sum(string)
+  string.extend(StringCalculator).add
 end
 
 describe StringCalculator do
-  it 'returns 0 for empty string' do
-    ''.should add_to 0
-  end
-
-  context '1 number' do
-    it 'returns 1 for 1' do
-      '1'.should add_to 1
-    end
-
-    it 'returns 5 for 5' do
-      '5'.should add_to 5
-    end
-
-    it 'return 7 for 7' do
-      '7'.should add_to 7
-    end
-
-    it 'return 10 for 10' do
-      '10'.should add_to 10
-    end
-  end
-
-  context '2 numbers' do
-    it 'returns 11 for 10,1' do
-      '10,1'.should add_to 11
-    end
-
-    it 'returns 13 for 10,3' do
-      '10,3'.should add_to 13
-    end
-
-    it 'return 15 for 10,5' do
-      '10,5'.should add_to 15
+  it 'return sum of positive numbers' do
+    cases = [
+     ['',0],
+     ['1',1],
+     ['5',5],
+     ['7',7],
+     ['10',10],
+     ['10,1',11],
+     ['10,3',13],
+     ['10,5',15],
+     ['100,20,3',123],
+     ['100,50,6',156],
+     ["1\n2",3],
+     ["1\n2,3",6],
+     ["//;\n2;3;4",9],
+     ["//@\n3@4@5",12],
+    ].each do |string,expected|
+      sum(string).should == expected
     end
   end
 
-  context '3 numbers' do
-    it 'returns 123 for 100,20,3' do
-      '100,20,3'.should add_to 123
-    end
-
-    it 'returns 156 for 100,50,6' do
-      '100,50,6'.should add_to 156
-    end
-  end
-
-  context 'delimiters' do
-    it 'allow newline as delimiter' do
-      "1\n2".should add_to 3
-    end
-
-    it 'allow both delimiters \n and ,' do
-      "1,2\n3".should add_to 6
-    end
-
-    it 'allow delimiter definition' do
-      "//;\n1;2;3".should add_to 6
-    end
-
-    it 'use @ as alternate delimiter' do
-     "//@\n1@2@3".should add_to 6
-    end
-  end
-
-  context 'negative numbers are not allowed' do
-    it 'raise error when contains negative number' do
-      lambda{"-1".extend(StringCalculator).add}.should raise_error
-    end
-
-    it 'specify negative numbers in error message' do
-      lambda{'1,-2,3'.extend(StringCalculator).add}.should raise_error 'negative numbers: -2'
-    end
-
-    it 'some negatives' do
-      lambda{'-1,2,-3'.extend(StringCalculator).add}.should raise_error 'negative numbers: -1, -3'
+  it 'specify negative numbers in error message' do
+    cases = [
+      ['-1', '-1'],
+      ['1,-2,3', '-2'],
+      ['-1,2,-3','-1, -3']
+    ].each do |string,negatives|
+      lambda{sum(string)}.should raise_error "negative numbers: #{negatives}"
     end
   end
 end
